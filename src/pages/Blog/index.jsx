3fad08dcf9e01d9fe09a0874c389a4b1ad51cde6
brandeddavid <david.mathenge98@gmail.com>
2018-10-07 12:44:34 +0300

    [Feature]render all posts on blog page

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getPosts } from '../../utils/api';
+import './Blog.css';
 
 class Blog extends Component {
   constructor(props) {
@@ -17,7 +18,22 @@ class Blog extends Component {
     });
   }
   render() {
-    return <React.Fragment />;
+    const { posts } = this.state;
+    const postCards = posts.map(post => {
+      return (
+        <div className="card post-card" key={post.id}>
+          <div className="card-body">
+            <h3 className="card-title">{post.title}</h3>
+            <div className="card-text">{post.body}</div>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <React.Fragment>
+        <div className="container">{postCards}</div>
+      </React.Fragment>
+    );
   }
 }
 
