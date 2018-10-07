7f9f4a2e0b4f8f3396ac7d9230d4f3e4a6aa94ff
brandeddavid <david.mathenge98@gmail.com>
2018-10-07 10:54:59 +0300

    [Feature]Setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
